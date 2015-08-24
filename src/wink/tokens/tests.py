c35c28b98896155f4370d5dca6bc0e1e3cf25430
sacherus <sacherus@gmail.com>
2015-08-24 22:32:28 +0200

    checking if get correct user in response after registration

--- src/wink/tokens/tests.py
+++ src/wink/tokens/tests.py
@@ -1,4 +1,3 @@
-# -*- coding: UTF-8 -*-
 from common.test_utils import APITestClientLogin
 from users.models import User
 
@@ -61,15 +60,6 @@ class FacebookTestCase(APITestClientLogin):
     '''
 
     # token should be valid till October 22nd, However facebook can revoke access
-
-    # EXTENDED_FACEBOOK_TOKEN1 = {
-    #     'backend': 'facebook',
-    #     'social_token': "CAAUshhSyCnEBAIXwDP2osd01os2pO0bKUoSgdkVxiiaZBdS1KBs8lZBO3ZCNFsWB7RPyTVuY0V6A4NnmeL2e1kYzexukL0TiCK3H3PzW2oSC0SYMYVHorXQIF8DquPipRhzhLJsixEOQNGsUw0clHVIHSByxQ2apuZA7jmB976dzyhK7CvnL6rptiyI7tCYZD}"}
-    # EXTENDED_FACEBOOK_TOKEN2 = {
-    #     'backend': 'facebook',
-    #     'social_token': "CAAUshhSyCnEBAPaxXFXwG2UaEWuEe6mkkU9o7pXNJZCTlSMP1J5kHEnPL1tSODbLE75jZBlv1mC3c4zrXfewKT7eIi26cqbce1IFv5k7oSGCYYwZCOWMEAoIEYVrQyfo6qs3t2cVDZCCxStCwrU2keQKhVouzKL9RfYlPzD65Dm2JAf8hwxF37FuzWKZBQg4ZD"}
-    # INVALID_FACEBOOK_TOKEN = {'backend': 'facebook', 'social_token': "1"}
-
     EXTENDED_FACEBOOK_TOKEN1 = "CAAUshhSyCnEBAIXwDP2osd01os2pO0bKUoSgdkVxiiaZBdS1KBs8lZBO3ZCNFsWB7RPyTVuY0V6A4NnmeL2e1kYzexukL0TiCK3H3PzW2oSC0SYMYVHorXQIF8DquPipRhzhLJsixEOQNGsUw0clHVIHSByxQ2apuZA7jmB976dzyhK7CvnL6rptiyI7tCYZD"
     EXTENDED_FACEBOOK_TOKEN2 = "CAAUshhSyCnEBAPaxXFXwG2UaEWuEe6mkkU9o7pXNJZCTlSMP1J5kHEnPL1tSODbLE75jZBlv1mC3c4zrXfewKT7eIi26cqbce1IFv5k7oSGCYYwZCOWMEAoIEYVrQyfo6qs3t2cVDZCCxStCwrU2keQKhVouzKL9RfYlPzD65Dm2JAf8hwxF37FuzWKZBQg4ZD"
     INVALID_FACEBOOK_TOKEN = "1"
@@ -80,7 +70,7 @@ class FacebookTestCase(APITestClientLogin):
     FACEBOOK_USER_DATA = {
         'email': 'sacherus@gmail.com',
         'display_name': '',
-        # 'username': 'Piotr Józef Kowenzowski',
+        'username': u'Piotr J\xf3zef Kowenzowski',
     }
 
     def setUp(self):
@@ -89,27 +79,27 @@ class FacebookTestCase(APITestClientLogin):
         self.EXTENDED_FACEBOOK_TOKEN2 = self.__token2dict(self.EXTENDED_FACEBOOK_TOKEN2)
         self.INVALID_FACEBOOK_TOKEN = self.__token2dict(self.INVALID_FACEBOOK_TOKEN)
 
-
-    def __compare_user2json(self, user, json):
-        self.assertEquals(user.display_name, self.FACEBOOK_USER_DATA['email'])
-        self.assertEquals(user.email, self.FACEBOOK_USER_DATA['email'])
-        self.assertEquals(user.username, self.FACEBOOK_USER_DATA['username'])
+    def __compare_user2json(self, user, data):
+        self.assertEquals(user.display_name, data['display_name'])
+        self.assertEquals(user.email, data['email'])
+        self.assertEquals(user.username, data['username'])
 
     def __token2dict(self, token):
         return {'backend': 'facebook', 'social_token': token, 'client_id': self.app.client_id,
                 'client_secret': self.app.client_secret}
 
-    def test_user_signup(self):
+    def test_user_signup_db(self):
         '''
         sent token and check if user is registered in database
         '''
         response = self.client.post(self.REGISTRATION_URL, data=self.EXTENDED_FACEBOOK_TOKEN1)
         user = User.objects.get(pk=2)
+        self.__compare_user2json(user, self.FACEBOOK_USER_DATA)
         self.check_valid_token(response.data)
 
     def test_user_signin(self):
         '''
-        sent token and check if user is returne
+        sent token and check if user is returned
         '''
         response = self.client.post(self.LOGIN_URL, data=self.EXTENDED_FACEBOOK_TOKEN1)
         self.check_valid_token(response.data)
@@ -122,6 +112,16 @@ class FacebookTestCase(APITestClientLogin):
         self.check_valid_token(response.data)
         response = self.client.post(self.LOGIN_URL, data=self.EXTENDED_FACEBOOK_TOKEN1)
         self.check_valid_token(response.data)
+
+    def test_user_signup_response(self):
+        '''
+        sent token, register, and then login using different token, pretty simple? :)
+        '''
+        response = self.client.post(self.REGISTRATION_URL, data=self.EXTENDED_FACEBOOK_TOKEN1)
+        self.check_valid_token(response.data)
+        facebook_set = set(self.FACEBOOK_USER_DATA.items())
+        response_set = set(response.data.items())
+        self.assertTrue(facebook_set.issubset(response_set), "Got real data from facebook in response after registration")
 
     def test_user_signup_twice(self):
         '''
@@ -137,7 +137,6 @@ class FacebookTestCase(APITestClientLogin):
         application sents invalid token...
         '''
         response = self.client.post(self.REGISTRATION_URL, data=self.INVALID_FACEBOOK_TOKEN)
-        print response
         self.assertAPIValidationErrorHasKey(response, "400 Client Error: Bad Request when connecting to facebook")
 
     def test_app_sent_invalid_token_login(self):
