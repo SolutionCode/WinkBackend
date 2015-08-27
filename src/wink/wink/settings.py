bf64717eb9c94977d3aa9f6f712c3320914fae1d
Lukasz Dziedzia <lukasz.dziedzia@cherrygroup.com>
2015-08-27 17:29:47 +0200

    pg db

--- src/wink/wink/settings.py
+++ src/wink/wink/settings.py
@@ -181,13 +181,16 @@ WSGI_APPLICATION = 'wink.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+database_url = os.getenv('DATABASE_URL', None)
+if database_url:
+    DATABASES['default'] = dj_database_url.config()
 
 
 # Internationalization
